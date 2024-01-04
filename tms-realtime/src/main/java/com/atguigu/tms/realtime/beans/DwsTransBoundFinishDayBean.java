d9a97283e0182d677c761989cbede7581ff74dbd
lcc-666 <2502247952@qq.com>
2024-01-04 17:08:30 +0800

    物流域转运完成实体类

---
+++ tms-realtime/src/main/java/com/atguigu/tms/realtime/beans/DwsTransBoundFinishDayBean.java
@@ -0,0 +1,19 @@
+package com.atguigu.tms.realtime.beans;
+
+import lombok.Builder;
+import lombok.Data;
+/**
+ * 物流域转运完成实体类
+ */
+@Data
+@Builder
+public class DwsTransBoundFinishDayBean {
+    // 统计日期
+    String curDate;
+
+    // 转运完成次数
+    Long boundFinishOrderCountBase;
+
+    // 时间戳
+    Long ts;
+}
