9c160157a3b2c8a65aafebd10768e50998ddf08a
lcc-666 <2502247952@qq.com>
2024-01-05 21:01:32 +0800

    SpringBoot环境初始化

---
+++ tms-publisher/src/main/java/com/atguigu/tms/publisher/TmsPublisherApplication.java
@@ -0,0 +1,13 @@
+package com.atguigu.tms.publisher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TmsPublisherApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TmsPublisherApplication.class, args);
+    }
+
+}
