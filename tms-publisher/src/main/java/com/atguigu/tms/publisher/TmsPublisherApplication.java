3139d86449df10fe97310d121aeb75fa2afdb005
lcc-666 <2502247952@qq.com>
2024-01-06 17:03:02 +0800

    SpringBoot主程序

--- tms-publisher/src/main/java/com/atguigu/tms/publisher/TmsPublisherApplication.java
+++ tms-publisher/src/main/java/com/atguigu/tms/publisher/TmsPublisherApplication.java
@@ -1,9 +1,11 @@
 package com.atguigu.tms.publisher;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan(basePackages = "com.atguigu.tms.publisher.mapper")
 public class TmsPublisherApplication {
 
     public static void main(String[] args) {
