f01717cdf0ddf1e76b523191bcd79cb4a4b2c1f0
lcc-666 <2502247952@qq.com>
2024-01-08 09:49:46 +0800

    各个省份的分拣数实体类

---
+++ tms-publisher/src/main/java/com/atguigu/tms/publisher/beans/BoundSortBean.java
@@ -0,0 +1,11 @@
+package com.atguigu.tms.publisher.beans;
+
+import lombok.Data;
+
+@Data
+public class BoundSortBean {
+    String org_name;
+    String city_name;
+    String province_name;
+    Long sort_count;
+}
